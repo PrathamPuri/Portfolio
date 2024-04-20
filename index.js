27eb9ee51edbf10fd83dcf14f1469fc5cbb83678
PrathamPuri <147255831+PrathamPuri@users.noreply.github.com>
2024-04-20 22:49:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ dayNight.addEventListener("click", () => {
     banner.classList.toggle("night");
 });
 
-let typingEffect=new Typed("#text",{
-    strings:["Web Developer","Coder"],
-    loop:true,
-    typingSpeed:100,
-    backSpeed:50,
-    backDelay:1000
+let typingEffect = new Typed("#text", {
+    strings: ["Web Developer", "Coder"],
+    loop: true,
+    typeSpeed: 30,       // Slower typing speed (characters per second)
+    backSpeed: 15,       // Slower backspacing speed (characters per second)
+    startDelay: 1500,    // Delay before typing starts (in milliseconds)
+    backDelay: 2000      // Delay before backspacing starts (in milliseconds)
 });
+
 
